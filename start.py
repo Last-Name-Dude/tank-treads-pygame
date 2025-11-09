0eacbe71945fbdc0090ed71e721f17f7c8896d4d
Last-Name-Dude <silvererm05@gmail.com>
2025-11-09 18:25:09 +0200

    Lisasin tank klassi ja koos sellega teise tanki. Liigutasin suurema osa peamisest tsüklist välja klassi definitsioonidesse, et neid saaks iga tanki kohta eraldi esile kutsuda ja ei peaks ühte ja sama koodi mitu korda kirjutama. Muutsin ka pildi suurust, et sellega oleks mugavam töötada.

--- start.py
+++ start.py
@@ -1,25 +1,21 @@
-# Example file showing a circle moving on screen
 import pygame
 from math import *
 
-# pygame setup
 pygame.init()
 
 screen = pygame.display.set_mode((1280, 720))
-tank_img = pygame.transform.scale(pygame.image.load("Tank.png"),(200,200))
+tank_img = pygame.transform.scale(pygame.image.load("Tank.png"),(150,150))
 
 tank_vector = pygame.Vector2()
 tank_angle = 0
 clock = pygame.time.Clock()
 dt = 0
+kuuli_kiirus = 300
+ekraani_keskkoht = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
-delay = 0
-bullet_list = []
+all_bullets = []
 
-
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-
-def transform_bilt_center(surf, img, pos, angle, vec):
+def transform_bilt_center(surf, img, pos, angle, vec): #selle funktsiooniga manipuleerime pilte nende nurga ja positsiooni põhjal, ilma neid moonutamata
     rotated = pygame.transform.rotate(img,angle)
     e = pygame.Vector2()
     e.x = pos.x - (rotated.get_rect()[3])/2 - vec.x * 30
@@ -27,8 +23,56 @@ def transform_bilt_center(surf, img, pos, angle, vec):
     surf.blit(rotated, e)
 
 running = True
+
+class tank: #kuna tahame, et ekraanil oleks mitu tanki loome tanki klassi
+    bullets = []
+    delay = 0
+    def __init__(self,pos,angle,vector,binds):
+        self.pos = pos
+        self.angle = angle
+        self.vector = vector
+        self.binds = binds
+
+    def update(self, dt):
+        self.vector.xy = sin(self.angle/180*pi), cos(self.angle/180*pi)
+        if self.delay > 0:
+            self.delay -= 100*dt
+
+    def check_input(self,dt,keys):
+        if keys[self.binds[0]]:
+            self.pos.x -= 300 * dt * self.vector.x
+            self.pos.y -= 300 * dt * self.vector.y
+        if keys[self.binds[2]]:
+            self.pos.x += 300 * dt * self.vector.x
+            self.pos.y += 300 * dt * self.vector.y
+        if keys[self.binds[1]]:
+            self.angle += 90 * dt
+        if keys[self.binds[3]]:
+            self.angle -= 90 * dt
+        if keys[self.binds[4]] and self.delay <= 0:
+            kuuli_algpunkt = pygame.Vector2(self.pos[:])
+            kuuli_algvektor = pygame.Vector2(self.vector[:])
+            kuuli_algpunkt -= kuuli_algvektor*60 #offset, et näeks välja nagu kuul tuleks torust
+            self.bullets.append([kuuli_algpunkt,kuuli_algvektor,10]) #asukoht, sihivektor ja eluaeg
+            self.delay = 50
+
+    def draw(self,screen, img):
+        transform_bilt_center(screen, img, self.pos, self.angle, self.vector)
+
+    def update_bullets(self):
+        for bullet in self.bullets: #vaatab üle kõik kuulid järjendis ja liigutab neid ja vajadusel ka kustutab
+            bullet[0].x -= bullet[1].x * dt * kuuli_kiirus
+            bullet[0].y -= bullet[1].y * dt * kuuli_kiirus
+            bullet[2] -= dt * 10
+            if bullet[2] <= 0:
+                self.bullets.remove(bullet)
+        return(self.bullets)
+
+
+tank1 = tank(ekraani_keskkoht,0,pygame.Vector2(),[pygame.K_w,pygame.K_a,pygame.K_s,pygame.K_d,pygame.K_SPACE])
+tank2 = tank(ekraani_keskkoht + pygame.Vector2(200,0),0,pygame.Vector2(),[pygame.K_UP,pygame.K_LEFT,pygame.K_DOWN,pygame.K_RIGHT,pygame.K_RCTRL])
+
 while running:
-    # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -37,41 +81,22 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
-    tank_vector.xy = sin(tank_angle/180*pi), cos(tank_angle/180*pi) # tanki suunaline ühikvektor
+    keys = pygame.key.get_pressed() #nupud
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.x -= 300 * dt * tank_vector.x
-        player_pos.y -= 300 * dt * tank_vector.y
-    if keys[pygame.K_s]:
-        player_pos.x += 300 * dt * tank_vector.x
-        player_pos.y += 300 * dt * tank_vector.y
-    if keys[pygame.K_a]:
-        tank_angle += 90 * dt
-    if keys[pygame.K_d]:
-        tank_angle -= 90 * dt
+    tank1.update(dt)
+    tank1.check_input(dt,keys)
+    tank1.draw(screen,tank_img)
 
-    if keys[pygame.K_SPACE] and delay == 0:
-        bullet_list.append([pygame.Vector2(player_pos[:]),pygame.Vector2(tank_vector[:]),10]) #asukoht, sihivektor ja eluaeg
-        delay = 10
+    tank2.update(dt)
+    tank2.check_input(dt,keys)
+    tank2.draw(screen,tank_img)
 
-    if delay > 0:
-        delay -= 1
+    for bullet in tank1.update_bullets() + tank2.update_bullets():
+        pygame.draw.circle(screen, "black", bullet[0], 7)
 
-    for bullet in bullet_list:
-        bullet[0].x -= bullet[1].x *10
-        bullet[0].y -= bullet[1].y *10
-        pygame.draw.circle(screen, "black", bullet[0], 10)
-        bullet[2] -= dt * 10
-        if bullet[2] <= 0:
-            bullet_list.remove(bullet)
-
-    transform_bilt_center(screen, tank_img, player_pos, tank_angle, tank_vector)
-
-    # flip() the display to put your work on screen
     pygame.display.flip()
 
-    # limits FPS to 60
+    # piirab FPS 60
     dt = clock.tick(60) / 1000
 
 pygame.quit()
